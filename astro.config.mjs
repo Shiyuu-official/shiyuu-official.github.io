7527454e245f1dd7105ed7b0c151dbed74a6d7d5
Shiyuu <241880474@smail.nju.edu.cn>
2026-01-13 23:07:30 +0800

    尝试修复GitHub Pages的子路径陷阱

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Shiyuu-official.github.io',
-	base: '/my-blog',
+	base: '/my-blog/',
 	integrations: [mdx(), sitemap()],
 });
